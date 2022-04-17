08d131b91cec47abb1e3d555572c0552428d229b
Dan Kim <danchanyoungkim@gmail.com>
2022-04-17 14:28:19 -0400

    did draft of thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -9,9 +9,35 @@ const thoughtSchema = new Schema(
             required: true,
             minLength: 1,
             maxLength: 280,
+            trim: true,
         },
         createdAt: {
-
-        }
+            type: Date,
+            default: Date.now,
+            get: (timestamp) = dateFormat(timestamp),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema]
+    },
+    // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
+    // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true,
+        },
+        id: false,
     }
-)
+);
+
+// Virtual to retrieve the length of the reactions.
+reactionSchema.virtual('reactionCount').get(function() {
+    return this.reaction.lengths;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
