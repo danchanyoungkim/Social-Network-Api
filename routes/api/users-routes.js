406415f88bb3b0f9d6d106a11519a4684b1decf5
Dan Kim <danchanyoungkim@gmail.com>
2022-04-19 04:41:00 -0400

    finished draft for users

--- routes/api/users-routes.js
+++ routes/api/users-routes.js
@@ -4,5 +4,15 @@ const {
     getSingleUser,
     createUser,
     updateUser,
-    deleteUser
-} = require('../../controllers/user-controller')
+    deleteUser,
+    addFriend,
+    removeFriend
+} = require('../../controllers/user-controller');
+
+router.route('/').get(getUsers).post(createUser);
+
+router.route('/:id').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+router.route('/:userId/friends/:friendId').post(addFriend).delete(removeFriend);
+
+module.exports = router;
